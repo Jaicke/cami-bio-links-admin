40bc5ef905ac935534351748817714dce3a2444f
Jaicke <jaicke-santos@outlook.com>
2022-08-07 14:54:30 -0300

    Adds order field to links

--- app/avo/resources/link_resource.rb
+++ app/avo/resources/link_resource.rb
@@ -7,9 +7,10 @@ class LinkResource < Avo::BaseResource
 
   field :bio_link, as: :belongs_to
 
-  field :id, as: :id, as: :hidden
-  field :title, as: :text, name: 'Título', sortable: true
-  field :url, as: :text, name: 'URL'
+  field :id, as: :id, as: :hidden, required: true
+  field :title, as: :text, name: 'Título', required: true, sortable: true
+  field :url, as: :text, name: 'URL', required: true
+  field :order, as: :number, name: 'Ordem', required: true
   field :active, as: :boolean, name: 'Ativo'
 
   # Actions
