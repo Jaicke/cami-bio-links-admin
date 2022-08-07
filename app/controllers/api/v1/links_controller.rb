ce6ee945d6fa7c4aa48069ced04de5251a64b270
Jaicke <jaicke-santos@outlook.com>
2022-08-07 19:04:34 -0300

    Fixes links ordering

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::LinksController < Api::V1::ApiController
   def index
-    links = Link.unscoped.by_user(@current_user).active
+    links = Link.unscoped.by_user(@current_user).active.ordered
 
     render json: links, status: :ok
   end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,10 +1,11 @@
 class Link < ApplicationRecord
-  default_scope { by_user(::Avo::App.context[:user]) }
+  default_scope { by_user(::Avo::App.context[:user]).ordered }
 
   belongs_to :bio_link, inverse_of: :links
 
-  validates :order, uniqueness: true
+  validates :order, uniqueness: { scope: :bio_link_id }
 
   scope :by_user, -> (user) { includes(:bio_link).where(bio_links: { user_id: user.id }) }
   scope :active, -> { where(active: true) }
+  scope :ordered, -> { order(order: :asc) }
 end
